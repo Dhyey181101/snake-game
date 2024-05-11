c2ae32856407120fd7c4e7c182215b9dc940849c
Dhyey Samirbhai Shah <dshah22@ncsu.edu>
2024-05-11 14:36:22 -0400

    Update MainMenu.js

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -6,7 +6,6 @@ const MainMenu = ({ onStartGame, onSettings }) => {
     <div className="main-menu">
       <h1>Snake Game</h1>
       <button onClick={onStartGame}>Play</button>
-      <button onClick={onSettings}>Settings</button>
     </div>
   );
 };
